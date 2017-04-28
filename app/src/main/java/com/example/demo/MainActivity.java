502c59b1f7fc0884dfa0f5efffc903ad222ce73c
phanz <phanz@qq.com>
2017-04-28 23:20:48 +0800

    RecyclerView简单实用及各种布局设置

--- app/src/main/java/com/example/demo/MainActivity.java
+++ app/src/main/java/com/example/demo/MainActivity.java
@@ -3,15 +3,16 @@ package com.example.demo;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.demo.aidl.client.BinderClientActivity;
+import com.example.demo.recyclerview.activity.RecyclerViewActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button mAidlBtn;
+    private Button mRecyclerBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +24,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent aidlIntent = new Intent(MainActivity.this, BinderClientActivity.class);
                 startActivity(aidlIntent);
+            }
+        });
+
+        mRecyclerBtn = (Button)findViewById(R.id.recycler_view_btn);
+        mRecyclerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent recyclerViewIntent = new Intent(MainActivity.this, RecyclerViewActivity.class);
+                startActivity(recyclerViewIntent);
             }
         });
     }
