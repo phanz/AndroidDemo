f4a64793db725031d1161cc1b63ec75d9d1736c3
phanz <phanz@qq.com>
2017-04-29 17:50:26 +0800

    ExpandableListView的基本用法

--- app/src/main/java/com/example/demo/MainActivity.java
+++ app/src/main/java/com/example/demo/MainActivity.java
@@ -8,12 +8,13 @@ import android.widget.Button;
 
 import com.example.demo.aidl.client.BinderClientActivity;
 import com.example.demo.customview.CustomViewActivity;
+import com.example.demo.expandablelistview.ExpandableListViewActivity;
 import com.example.demo.fragment.FragmentActivity;
 import com.example.demo.gesture.SimpleGestureActivity;
 import com.example.demo.recyclerview.activity.ItemDecorationActivity;
 import com.example.demo.recyclerview.activity.RecyclerViewActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Button mAidlBtn;
     private Button mRecyclerBtn;
@@ -21,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     private Button mFragmentBtn;
     private Button mGestureBtn;
     private Button mCustomViewBtn;
+    private Button mExpandableListViewBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,57 +30,68 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mAidlBtn = (Button)findViewById(R.id.aidl_btn);
-        mAidlBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent aidlIntent = new Intent(MainActivity.this, BinderClientActivity.class);
-                startActivity(aidlIntent);
-            }
-        });
+        mAidlBtn.setOnClickListener(this);
 
         mRecyclerBtn = (Button)findViewById(R.id.recycler_view_btn);
-        mRecyclerBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent recyclerViewIntent = new Intent(MainActivity.this, RecyclerViewActivity.class);
-                startActivity(recyclerViewIntent);
-            }
-        });
+        mRecyclerBtn.setOnClickListener(this);
 
         mItemDecoration = (Button)findViewById(R.id.item_decoration_btn);
-        mItemDecoration.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent itemDecorationIntent = new Intent(MainActivity.this, ItemDecorationActivity.class);
-                startActivity(itemDecorationIntent);
-            }
-        });
+        mItemDecoration.setOnClickListener(this);
 
         mFragmentBtn = (Button)findViewById(R.id.fragment_btn);
-        mFragmentBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent fragmentIntent = new Intent(MainActivity.this, FragmentActivity.class);
-                startActivity(fragmentIntent);
-            }
-        });
+        mFragmentBtn.setOnClickListener(this);
 
         mGestureBtn = (Button)findViewById(R.id.gesture_btn);
-        mGestureBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent gestureIntent = new Intent(MainActivity.this, SimpleGestureActivity.class);
-                startActivity(gestureIntent);
-            }
-        });
+        mGestureBtn.setOnClickListener(this);
 
         mCustomViewBtn = (Button)findViewById(R.id.custom_view_btn);
-        mCustomViewBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        mCustomViewBtn.setOnClickListener(this);
+
+        mExpandableListViewBtn = (Button)findViewById(R.id.expandable_list_view_btn);
+        mExpandableListViewBtn.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch (id){
+            case R.id.aidl_btn:
+                Intent aidlIntent = new Intent(MainActivity.this, BinderClientActivity.class);
+                startActivity(aidlIntent);
+                break;
+
+            case R.id.recycler_view_btn:
+                Intent recyclerViewIntent = new Intent(MainActivity.this, RecyclerViewActivity.class);
+                startActivity(recyclerViewIntent);
+                break;
+
+            case R.id.item_decoration_btn:
+                Intent itemDecorationIntent = new Intent(MainActivity.this, ItemDecorationActivity.class);
+                startActivity(itemDecorationIntent);
+                break;
+
+            case R.id.fragment_btn:
+                Intent fragmentIntent = new Intent(MainActivity.this, FragmentActivity.class);
+                startActivity(fragmentIntent);
+                break;
+
+            case R.id.gesture_btn:
+                Intent gestureIntent = new Intent(MainActivity.this, SimpleGestureActivity.class);
+                startActivity(gestureIntent);
+                break;
+
+            case R.id.custom_view_btn:
                 Intent customViewIntent = new Intent(MainActivity.this, CustomViewActivity.class);
                 startActivity(customViewIntent);
-            }
-        });
+                break;
+
+            case R.id.expandable_list_view_btn:
+                Intent expandableListViewIntent = new Intent(MainActivity.this, ExpandableListViewActivity.class);
+                startActivity(expandableListViewIntent);
+                break;
+            default:
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/demo/expandablelistview/MyExpandableListAdapter.java
@@ -0,0 +1,109 @@
+package com.example.demo.expandablelistview;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.example.demo.R;
+
+/**
+ * Created by hanzai.peng on 2017/4/7.
+ */
+
+public class MyExpandableListAdapter extends BaseExpandableListAdapter {
+
+    private String[] category = new String[]{"僵尸  ", "魔法植物", "远程植物","僵尸  "};
+
+    private String[][] subCategory = new String[][]{
+            {"旗帜僵尸", "铠甲僵尸", "书生见识", "铁桶僵尸", "尸娃僵尸", "舞蹈僵尸"},
+            {"黄金蘑菇", "贪睡蘑菇", "大头蘑菇", "诱惑植物", "多嘴蘑菇", "七彩蘑菇"},
+            {"满天星", "风车植物", "带刺植物", "贪睡植物", "双子植物", "胆怯蘑菇"},
+            {"旗帜僵尸", "铠甲僵尸", "书生见识", "铁桶僵尸", "尸娃僵尸", "舞蹈僵尸"}
+
+    };
+
+    private Context mContext;
+
+    public MyExpandableListAdapter(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded,
+                             View convertView, ViewGroup parent) {
+
+        RelativeLayout layout = (RelativeLayout) LayoutInflater.from(mContext)
+                .inflate(R.layout.group_expandable_list_view, null);
+        TextView textView = (TextView) layout.findViewById(R.id.category);
+        textView.setText(category[groupPosition]);
+        ImageView arrowImg = (ImageView)layout.findViewById(R.id.arrowImage);
+
+        if(groupPosition % 2 == 0){
+            layout.setBackgroundColor(Color.GRAY);
+        }
+        if(isExpanded){
+            arrowImg.setBackgroundResource(R.drawable.arrow_up);
+        }else{
+            arrowImg.setBackgroundResource(R.drawable.arrow_down);
+        }
+        return layout;
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return category.length;
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return category[groupPosition];
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return subCategory[groupPosition].length;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition,
+                             boolean isLastChild, View convertView, ViewGroup parent) {
+
+        RelativeLayout layout = (RelativeLayout) LayoutInflater.from(mContext)
+                .inflate(R.layout.item_expandable_list_view, null);
+        TextView textView = (TextView) layout.findViewById(R.id.sub_category);
+        textView.setText(subCategory[groupPosition][childPosition]);
+
+        return layout;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return subCategory[groupPosition][childPosition];
+    }
+}
